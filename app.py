c26b89e2a532b1813575bfc05b565dd0c6bee5e6
Nastya Bespalova <90254547+bspnastya@users.noreply.github.com>
2025-06-20 17:29:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -422,7 +422,7 @@ else:
                 }}
             }}, {TIME_LIMIT * 1000});
             </script>
-            """, height=300)
+            """, height=310)
         else:
             st.markdown(
                 "<div style='text-align:left;font-style:italic;color:#666;padding:40px 0;'>"
